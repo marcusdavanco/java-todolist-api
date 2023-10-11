8b94c471f9a1870bcd19f64f1cf83da8a1afebbc
Marcus Davanço <md.daves@gmail.com>
2023-10-11 12:25:08 -0300

    feat: authorization filter

--- src/main/java/br/com/marcusdavanco/todolist/user/UserController.java
+++ src/main/java/br/com/marcusdavanco/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.marcusdavanco.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,11 @@ public class UserController {
             System.out.println("Usuário já existe");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
         }
+
+        var passwordHashed = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
